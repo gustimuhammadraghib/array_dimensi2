d94d42e11a71c035b34780336aaa24f10b5ccbc5
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-01-03 00:54:21 +0800

    membuat variabel

--- array_dimensi2.cpp
+++ array_dimensi2.cpp
@@ -6,4 +6,32 @@ using namespace std;
 main()
  
 {
+	    int X[3][5],R[3][5],W[1][5],C[3][5],i,j,k;
+ 
+ cout<<"\n\t\t\tNAMA		 : GUSTI MUHAMMAD RAGHIB\n";
+
+cout<<"\t\t\tNPM 			 : 16630904\n";
+
+cout<<"\t\t\tPROGRAM 		 : TEKNIK INFORMATIKA\n";
+cout<<"\t\t\tTEKNIK 		 : INPUTAN ARRAY DIMENSI 2\n";
+ 
+    cout<<"INPUT NILAI MATRIX X : \n";
+ 
+   cout<<"------------------------- \n";
+ 
+   for(i=0;i<3;i++)
+ 
+   {
+ 
+        for(j=0;j<5;j++)
+ 
+        {
+ 
+        cout<<"matrik X "<<(i+1)<<","<<(j+1)<<" : ";
+ 
+      cin>>X[i][j];
+ 
+        }
+ 
+   }
 }
