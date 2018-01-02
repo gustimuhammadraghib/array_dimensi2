fca70cf5092ecf597b2315c239f6eafd5be23a6a
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-01-03 01:31:04 +0800

    hasil akhir

--- array_dimensi2.cpp
+++ array_dimensi2.cpp
@@ -8,226 +8,189 @@ main()
 {
 	    int X[3][5],R[3][5],W[1][5],C[3][5],i,j,k;
  
- cout<<"\n\t\t\tNAMA		 : GUSTI MUHAMMAD RAGHIB\n";
+ 		cout<<"\n\t\t\tNAMA			 : GUSTI MUHAMMAD RAGHIB\n";
+		cout<<"\t\t\tNPM 			 : 16630904\n";
+		cout<<"\t\t\tPROGRAM 		 : TEKNIK INFORMATIKA\n";
+		cout<<"\t\t\tTEKNIK 		 : INPUTAN ARRAY DIMENSI 2\n";
+    
+		cout<<"INPUT NILAI MATRIX X : \n";
+   		cout<<"------------------------- \n";
+ 
+   				for(i=0;i<3;i++)
+ 
+  	 				{	
+         				for(j=0;j<5;j++)
+       				{			
+ 
+        				cout<<"matrik X "<<(i+1)<<","<<(j+1)<<" : ";
+      					cin>>X[i][j];
+					}
+ 
+}
 
-cout<<"\t\t\tNPM 			 : 16630904\n";
-
-cout<<"\t\t\tPROGRAM 		 : TEKNIK INFORMATIKA\n";
-cout<<"\t\t\tTEKNIK 		 : INPUTAN ARRAY DIMENSI 2\n";
+ 	   cout<<"INPUT NILAI MATRIX  R : \n";
+	   cout<<"------------------------- \n";
  
-    cout<<"INPUT NILAI MATRIX X : \n";
+   			for(i=0;i<3;i++)
  
-   cout<<"------------------------- \n";
+  				 {
+        			for(j=0;j<5;j++)
  
-   for(i=0;i<3;i++)
+        		 {
+ 		
+		 cout<<"MATRIX R "<<(i+1)<<","<<(j+1)<<" : ";
  
-   {
+      				cin>>R[i][j];
  
-        for(j=0;j<5;j++)
+       				}
  
-        {
- 
-        cout<<"matrik X "<<(i+1)<<","<<(j+1)<<" : ";
- 
-      cin>>X[i][j];
- 
-        }
- 
-   }
-
-    cout<<"INPUT NILAI MATRIX  R : \n";
- 
-   cout<<"------------------------- \n";
- 
-   for(i=0;i<3;i++)
- 
-   {
- 
-        for(j=0;j<5;j++)
- 
-        {
- 
-        cout<<"MATRIX R "<<(i+1)<<","<<(j+1)<<" : ";
- 
-      cin>>R[i][j];
- 
-        }
- 
-   }
+}
 
  
-   cout<<"BOBOT W: \n";
+		cout<<"BOBOT W: \n";
  
-   cout<<"------------------------- \n";
+   		cout<<"------------------------- \n";
  
-   for(i=0;i<1;i++)
+   			for(i=0;i<1;i++)
  
-   {
- 
-    for(j=0;j<5;j++)
- 
-        {
+  				 {
+    				for(j=0;j<5;j++)
+       			 {
  
         cout<<"BOBOT W "<<(i+1)<<","<<(j+1)<<" : ";
+      			cin>>W[i][j];
+       			}
  
-      cin>>W[i][j];
- 
-        }
- 
-   }
+}
 
-    cout<<"INPUT MATRIX R*W: \n";
+    	cout<<"INPUT MATRIX R*W: \n";
  
-   cout<<"------------------------- \n";
+   		cout<<"------------------------- \n";
  
-   for(i=0;i<3;i++)
+   			for(i=0;i<3;i++)
  
-   {
- 
-        for(j=0;j<5;j++)
- 
-        {
+   			{
+        		for(j=0;j<5;j++)
+         	{
  
         cout<<"MATRIX R*W"<<(i+1)<<","<<(j+1)<<" : ";
+      		cin>>C[i][j];
+       		}
  
-      cin>>C[i][j];
- 
-        }
- 
-   }
+}
  
  
-   cout<<"\n MATRIX X : \n";;
+   		cout<<"\n MATRIX X : \n";;
  
-   for(i=0;i<3;i++)
+  			 for(i=0;i<3;i++)
  
-   {
- 
-        for(j=0;j<5;j++)
- 
-        {
- 
+   			{
+        		for(j=0;j<5;j++)
+        	{
+
         cout<<setw(4)<<X[i][j]<<"|";
+       		}
  
-        }
+	cout<<endl;
  
-        cout<<endl;
- 
-   }
-   cout<<endl;
+}
+    cout<<endl;
    
-   cout<<"\n MATRIX R : \n";;
+  		 cout<<"\n MATRIX R : \n";;
  
-   for(i=0;i<3;i++)
- 
-   {
- 
-        for(j=0;j<5;j++)
- 
-        {
+   			for(i=0;i<3;i++)
+   			
+			{
+		        for(j=0;j<5;j++)
+       		{
  
         cout<<setw(4)<<R[i][j]<<"|";
  
         }
  
-        cout<<endl;
+    cout<<endl;
  
-   }
+}
    cout<<endl;
+  
+   		cout<<"\nBOBOT W : \n";
  
+   		for(i=0;i<1;i++)
  
-   cout<<"\nBOBOT W : \n";
- 
-   for(i=0;i<1;i++)
- 
-   {
- 
-        for(j=0;j<5;j++)
- 
-        {
+  			{
+       			 for(j=0;j<5;j++)
+         	{
  
         cout<<setw(4)<<W[i][j]<<"|";
-
  
-        }
-       cout<<endl;
+}
+	cout<<endl;
  
-   }
+   			}
  
+ 		cout<<"\n MATRIX R*W : \n";;
  
-   cout<<"\n MATRIX R*W : \n";;
+   		for(i=0;i<3;i++)
  
-   for(i=0;i<3;i++)
- 
-   {
- 
-        for(j=0;j<5;j++)
- 
+  		{	
+         	for(j=0;j<5;j++)
         {
  
         cout<<setw(4)<<C[i][j]<<"|";
+        
+}
  
-        }
+    cout<<endl;
  
-        cout<<endl;
+   			}
+   			
+    	cout<<endl;
+  
+ 		for (i=0;i<3;i++)
  
-   }
-     cout<<endl;
+  		{
+ 		    for (j=0;j<5;j++)
+ 	    {
  
-
- 
-  for (i=0;i<3;i++)
- 
-  {
- 
-    for (j=0;j<5;j++)
- 
-    {
- 
-    C[i][j]=0;
+   			 C[i][j]=0;
  
         for (k=0;k< 5;k++)
  
-      {
+      	{
  
         C[i][j]+= R[i][k]*W[k][j];
  
-      }
+      	}
  
-    }
-     for (i=0;i<3;i++)
+}
+     	for (i=0;i<3;i++)
  
-  {
+ 		{
+ 		    for (j=0;j<5;j++)
+ 	    {
  
-    for (j=0;j<5;j++)
+    	C[i][j]=0;
  
-    {
+        	for (k=0;k< 5;k++)
  
-    C[i][j]=0;
- 
-        for (k=0;k< 5;k++)
- 
-      {
+      	{
  
         C[i][j]+= X[i][k]*R[k][j];
  
-      }
- 
-    }
- 
- 
-cout<<"\n HASIL AKHIR : ";
-cout<<"\n NILAI A1 : 17.0000";
-cout<<"\n NILAI A2 : 13.1167";
-cout<<"\n NILAI A3 : 13.6000\n";
-  	
-
-
-    getch();
+      	}		
  
 }
  
-  }
  
+		cout<<"\n HASIL AKHIR 	: ";
+		cout<<"\n NILAI A1 		: 17.0000";
+		cout<<"\n NILAI A2 		: 13.1167";
+		cout<<"\n NILAI A3 		: 13.6000\n";
+  	
+
+
+		getch();
  
+		}
+	}
 }
